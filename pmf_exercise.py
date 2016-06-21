1d13f1cf907829983e1e6748058a4172f2437dd8
Varun Pillai <pillai.varun@outlook.com>
2016-06-21 12:41:39 -0400

    first run

--- pmf_exercise.py
+++ pmf_exercise.py
@@ -5,9 +5,20 @@ def PrintPmf(pmf):
         print value, prob
 
 def ProbBigger(pmf1, pmf2):
-    new = Pmf.Pmf()
-    return new
+    total = 0
+    for v1, p1 in pmf1.Items():
+        for v2, p2 in pmf2.Items():
+            if v1 > v2:
+                total += p1*p2
+    return total
 
 six_sided_die = Pmf.MakePmfFromList([1, 2, 3, 4, 5, 6])
-
+ten_sided_die = Pmf.MakeHistFromList(range(1, 11))
 PrintPmf(six_sided_die)
+PrintPmf(ten_sided_die)
+
+print 'Prob ten-side is bigger: ', ProbBigger(ten_sided_die, six_sided_die)
+
+
+
+
